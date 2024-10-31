4c7355cc4d8964ed72c2263286734caf15bb4e0c
rethes <rethesmutua@gmail.com>
2024-10-31 12:58:01 +0300

    update walls.py

--- walls.py
+++ walls.py
@@ -3,6 +3,10 @@ import pygame
 class Wall:
     def __init__(self, x, y, width, height, color=(255, 0, 0)):
         self.rect = pygame.Rect(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
         self.color = color
 
     def draw(self, surface):
